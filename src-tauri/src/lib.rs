217d2deb117aed0ec2b395fe51f3f8256075978a
fomn16 <felipe.neves16@gmail.com>
2025-05-26 18:11:21 -0300

    adicionando integração SQLite

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,38 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri_plugin_sql::{Migration, MigrationKind};  
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![  
+        Migration {  
+            version: 1,  
+            description: "create tables",  
+            sql: "  
+                CREATE TABLE projects (
+                    id	INTEGER NOT NULL UNIQUE,
+                    name	TEXT NOT NULL UNIQUE,
+                    filesFrom	TEXT NOT NULL UNIQUE,
+                    filesTo	TEXT NOT NULL UNIQUE,
+                    PRIMARY KEY(id AUTOINCREMENT)
+                );
+
+                CREATE TABLE ignoredFiles (
+                    id	INTEGER NOT NULL UNIQUE,
+                    name	TEXT NOT NULL,
+                    projectId	INTEGER NOT NULL,
+                    PRIMARY KEY(id AUTOINCREMENT),
+                    CONSTRAINT FK_IgnoredFile_Project FOREIGN KEY(projectId) REFERENCES projects(id)
+                );
+            ",  
+            kind: MigrationKind::Up,  
+        }  
+    ]; 
+    
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new()
+            .add_migrations("sqlite:ANTTPublisher.db", migrations)
+            .build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        //.invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,130 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+import Database from "@tauri-apps/plugin-sql";
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+type Project = {
+  id: number;
+  name: string;
+  filesFrom: string;
+  filesTo: string;
+}
+
+type IgnoredFile = {
+  id: number;
+  name: string;
+  projectId: number;
+}
+
+function App() {
+  const [isLoadingProjects, setIsLoadingProjects] = useState(true);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [name, setName] = useState<string>("");
+  const [filesFrom, setFilesFrom] = useState<string>("");
+  const [filesTo, setFilesTo] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  async function getProjects() {
+    try {
+      const db = await Database.load("sqlite:ANTTPublisher.db");
+      const dbProjects = await db.select<Project[]>("SELECT * FROM projects");
+
+      setError("");
+      setProjects(dbProjects);
+      setIsLoadingProjects(false);
+    } catch (error) {
+      console.log(error);
+      setError("Erro ao ler projetos");
+    }
   }
+
+  async function setProject(project: Omit<Project, "id">) {
+    try {
+      setIsLoadingProjects(true);
+      const db = await Database.load("sqlite:ANTTPublisher.db");
+
+      await db.execute("INSERT INTO projects (name, filesFrom, filesTo) VALUES ($1, $2, $3)", [
+        project.name,
+        project.filesFrom,
+        project.filesTo
+      ]);
+
+      getProjects().then(() => setIsLoadingProjects(false));
+    } catch (error) {
+      console.log(error);
+      setError("Falha ao inserir projeto");
+    }
+  }
+
+  useEffect(()=>{
+    getProjects();
+  }, []);
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>Welcome to Tauri + SQLite</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      {isLoadingProjects ? (
+        <div>Carregando projetos...</div>
+      ) : (
+        <div className="collumns">
+          <form
+            className="row"
+            onSubmit={(e) => {
+              e.preventDefault();
+              setProject({ name, filesFrom, filesTo });
+              getProjects();
+            }}
+          >
+            <div className="collumns">
+              <input
+                id="name-input"
+                onChange={(e) => setName(e.currentTarget.value)}
+                placeholder="Digite o nome..."
+              />
+              <input
+                id="filesFrom-input"
+                onChange={(e) => setFilesFrom(e.currentTarget.value)}
+                placeholder="Digite a origem dos arquivos..."
+              />
+              <input
+                id="filesTo-input"
+                onChange={(e) => setFilesTo(e.currentTarget.value)}
+                placeholder="Digite o destino dos arquivos..."
+              />
+              <button type="submit">Adicionar Projeto</button>
+            </div>
+          </form>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+          <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+            <h1>Projetos</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>Origem dos Arquivos</th>
+                  <th>Destino dos Arquivos</th>
+                </tr>
+              </thead>
+              <tbody>
+                {projects.map((project) => (
+                  <tr key={project.id}>
+                    <td>{project.id}</td>
+                    <td>{project.name}</td>
+                    <td>{project.filesFrom}</td>
+                    <td>{project.filesTo}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+
+      {error && <p>{error}</p>}
     </main>
   );
 }
